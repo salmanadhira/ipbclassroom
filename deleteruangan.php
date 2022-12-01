b4a62ca1c4acd668fa0cb72839b005374615f74a
salmadanu <92136661+salmadanu@users.noreply.github.com>
2022-12-01 14:06:20 +0700

    Update deleteruangan.php

--- deleteruangan.php
+++ deleteruangan.php
@@ -1,22 +1,23 @@
 <?php
 include("config.php");
 // cek apakah tombol di form sudah diklik atau belum
-if(isset($_POST["deleteruangan"])){
+if(isset($_POST['deleteruangan'])){
 
+    // Ambil data dari $_POST
     $kode_ruangan = $_POST['kode_ruangan'];
-    var_dump($kode_ruangan);
-	// buat query
-    //$query3 = pg_query("DELETE FROM peminjaman WHERE kode_ruangan_peminjaman IN (SELECT kode_ruangan FROM ruangan WHERE kode_ruangan = $kode_ruangan)");
-    //$query2 = pg_query("DELETE FROM tiket WHERE id_kereta = $id_kereta");
-    $query = pg_query("DELETE FROM ruangan WHERE kode_ruangan = '$kode_ruangan'");
 
-	// apakah query simpan berhasil?
-	if( $query==TRUE ) {
-		// kalau berhasil alihkan ke halaman index.php 
-		header('Location: index.php?status3=hapusRuanganBerhasil');
+	// Delete semua data yang berhubungan dengan ruangan, dari yang paling kuat hingga paling lemah hingga kereta
+    //$query = pg_query("DELETE FROM peminjaman WHERE kode_ruangan_peminjaman IN (SELECT  FROM tiket WHERE id_kereta = $id_kereta)");
+    $query = pg_query("DELETE FROM peminjaman WHERE kode_ruangan_peminjaman = '$kode_ruangan'");
+    $query2 = pg_query("DELETE FROM ruangan WHERE kode_ruangan = '$kode_ruangan'");
+
+	// apakah query berhasil?
+	if( $query==TRUE && $query2==TRUE ) {
+		// kalau berhasil alihkan ke halaman index_admin.php 
+		header('Location: index_admin.php?status3=hapusRuanganBerhasil');
 	} else {
 		// kalau gagal kembalikan ke halaman form
-	    header('Location: daftarruangan_page.php?status4=hapusRuanganGagal');
+		header('Location: daftarruangan_page.php?status4=hapusRuanganGagal');
 	}
 
 
